e01bf173168fdd46d1a77ffe96d806ce65df5a8c
Mercyanu <mercyanu14@gmail.com>
2021-01-28 11:40:47 +0100

    homepage build up using few components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Homepage from './pages/homepage/homepage.component';
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div>
+      <Homepage/>
+    </div>
   }
 }
 
