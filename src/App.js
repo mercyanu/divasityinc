333f1b8b3927edef1c10cc9052e1e3529e978813
Mercyanu <mercyanu14@gmail.com>
2021-02-01 10:10:24 +0100

    Add Header component, route dynamically and further rework look and feel

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header-component';
 
 
 class App extends Component {
   render() {
     return <div>
+      <Header/>
       <Switch>
       <Route exact path='/' component={Homepage}/>
       <Route exact path='/shop' component={ShopPage}/>
